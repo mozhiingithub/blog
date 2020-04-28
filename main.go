4e8e9a3489986dc3efe02bd9dd46a145ea82495e
mo <245994626@qq.com>
2020-04-28 14:15:36 +0800

    添加’加载前端’语句。

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	engine := gin.Default() // 生成一个引擎
-	//engine.LoadHTMLGlob("template/*") // 加载前端
+	engine := gin.Default()                 // 生成一个引擎
+	engine.LoadHTMLGlob("template/*")       // 加载前端
 	for _, h := range handler.HandlerList { // 注册各类handler
 		engine.Handle(h.GetMethod(), h.GetPath(), h.GetHandle())
 	}
