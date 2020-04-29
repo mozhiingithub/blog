f28a3dec3c8e62212f428893fda21a85b2a55577
mo <245994626@qq.com>
2020-04-29 11:08:01 +0800

    完成博文展示页handler。

---
+++ handler/getArticle.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	d "../database"
+	"github.com/gin-gonic/gin"
+)
+
+var getArticle = handler{
+	path:   "/arcticle",
+	method: "GET",
+	handle: func(c *gin.Context) {
+		for {
+			articleIDStr := c.Query("id") // 获取博文id参数
+			if "" == articleIDStr {
+				break
+			}
+			articleID, err := strconv.Atoi(articleIDStr)
+			if nil != err {
+				break
+			}
+			title, ok := d.GetTitle(articleID) // 获取此id对应的博文标题
+			if !ok {
+				break
+			}
+			t, ok := d.GetTime(articleID) // 获取此id对应的博文发布时间
+			if !ok {
+				break
+			}
+			content, ok := d.GetContent(articleID) // 获取此id对应的博文正文
+			if !ok {
+				break
+			}
+			c.HTML(http.StatusOK, "article.html", gin.H{
+				"title":   title,
+				"t":       t,
+				"content": content,
+			})
+			return
+		}
+		c.HTML(http.StatusNotFound, "result.html", gin.H{
+			"result": "博文不存在",
+		})
+		return
+	},
+}
