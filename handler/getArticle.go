2a9e2e6ce14cf2749c1fb33d14a4fa909c43742d
mo <245994626@qq.com>
2020-04-30 17:23:26 +0800

    添加缓存机制。

--- handler/getArticle.go
+++ handler/getArticle.go
@@ -4,14 +4,19 @@ import (
 	"net/http"
 	"strconv"
 
+	ch "../cache"
 	d "../database"
 	"github.com/gin-gonic/gin"
+	"github.com/gomodule/redigo/redis"
 )
+
+const expireSecond = 300
 
 var getArticle = handler{
 	path:   "/article",
 	method: "GET",
 	handle: func(c *gin.Context) {
+		result := "博文不存在"
 		for {
 			articleIDStr := c.Query("id") // 获取博文id参数
 			if "" == articleIDStr {
@@ -21,6 +26,29 @@ var getArticle = handler{
 			if nil != err {
 				break
 			}
+
+			for { // 先判断redis中是否有此博文的缓存
+				var rs redis.Conn = ch.GetInstance() // 获取redis实例
+				ok, _ := redis.Bool(rs.Do("exists", articleID))
+				if !ok { // redis中没有该博文缓存
+					break
+				}
+				m, err := redis.StringMap(rs.Do("hgetall", articleID)) // 获取博文的标题、时间、正文
+				if 0 == len(m) {                                       // 获取失败
+					break
+				}
+				// 获取博文阅读量并加一。
+				// 假设count中没有该id的阅读量值，则新建一个并加一
+				count, err := redis.String(rs.Do("hincrby", "count", articleID))
+				if nil != err { // 获取失败
+					break
+				}
+				m["count"] = count                       // 将阅读量添加到m中
+				c.HTML(http.StatusOK, "article.html", m) // 将缓存传到博文展示页前端
+				return
+			}
+
+			// 因各种原因，无法获取博文缓存。现从数据库中获取
 			title, ok := d.GetTitle(articleID) // 获取此id对应的博文标题
 			if !ok {
 				break
@@ -33,15 +61,32 @@ var getArticle = handler{
 			if !ok {
 				break
 			}
+
+			// 从数据库中获取博文内容，将其写入缓存
+			var rs redis.Conn = ch.GetInstance()                                          // 获取redis实例
+			err = rs.Send("hmset", articleID, "title", title, "t", t, "content", content) // 以id为key，写入一个hash
+			if nil != err {                                                               // 写入失败
+				result = "博文写入缓存失败"
+				break
+			}
+			count, err := redis.String(rs.Do("hincrby", "count", articleID))
+			if nil != err { // 获取失败
+				result = "获取阅读量失败"
+				break
+			}
+			rs.Send("expire", articleID, expireSecond) // 为该博文的缓存设置过期时间
+
+			// 将内容传到博文展示页前端
 			c.HTML(http.StatusOK, "article.html", gin.H{
 				"title":   title,
 				"t":       t,
 				"content": content,
+				"count":   count,
 			})
 			return
 		}
 		c.HTML(http.StatusNotFound, "result.html", gin.H{
-			"result": "博文不存在",
+			"result": result,
 		})
 		return
 	},
